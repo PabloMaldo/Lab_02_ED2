093fb0865ca19b14b752da59a368196bd4aff912
PabloMaldo <85586916+PabloMaldo@users.noreply.github.com>
2023-07-28 22:09:42 -0600

    Create map.h

---
+++ map.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef MAP_H
+#define	MAP_H
+
+#include <xc.h> 
+
+int mapRange(int input, int inputMin, int inputMax, float outputMin, float outputMax);
+
+#endif	/* MAP_H */
